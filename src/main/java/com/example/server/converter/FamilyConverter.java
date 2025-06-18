94fe0b36604a755b31c6337d6ff903283b2f1cd4
LJYeon12 <juyeon010530@gmail.com>
2025-06-18 20:20:51 +0900

    rule 생성, 조회, 상세조회 구현 완료

--- src/main/java/com/example/server/converter/FamilyConverter.java
+++ src/main/java/com/example/server/converter/FamilyConverter.java
@@ -4,6 +4,7 @@ import com.example.server.domain.entity.Family;
 import com.example.server.domain.entity.FamilyMember;
 import com.example.server.domain.entity.FamilyMotto;
 import com.example.server.dto.culture.CultureRequestDTO;
+import jakarta.validation.Valid;
 
 public class FamilyConverter {
     public static FamilyMotto toFamilyMotto(CultureRequestDTO.MottoRequestDTO requestDTO, FamilyMember familyMember, Family family) {
@@ -11,6 +12,16 @@ public class FamilyConverter {
                 .title(requestDTO.getMotto())
                 .familyMember(familyMember)
                 .family(family)
+                .build();
+    }
+
+    public static FamilyMotto toFamilyRule(CultureRequestDTO.@Valid CreateRuleRequestDTO requestDTO, FamilyMember familyMember, Family family) {
+        return new FamilyMotto().builder()
+                .title(requestDTO.getTitle())
+                .familyMember(familyMember)
+                .family(family)
+                .description(requestDTO.getDescription())
+                .ruleType(requestDTO.getRuleType())
                 .build();
     }
 }
